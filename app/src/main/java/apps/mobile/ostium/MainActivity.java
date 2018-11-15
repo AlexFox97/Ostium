680c059400c222e02206102483bb710205e6855d
Alex Fox <1alexwfox@gmail.com>
2018-11-15 19:27:03 +0000

    Almost working calendar pull, need to test on live phone

--- app/src/main/java/apps/mobile/ostium/MainActivity.java
+++ app/src/main/java/apps/mobile/ostium/MainActivity.java
@@ -1,13 +1,82 @@
 package apps.mobile.ostium;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
+
+    final ListView list = (ListView) findViewById(R.id.list);
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+
+
+    private static final int MY_CAL_REQ = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+        list.setAdapter(adapter);
     }
+
+
+
+    public void getDataFromEventTable(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, MY_CAL_REQ);
+        }
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+
+        String[] mProjection =
+                {
+                        "_id",
+                        CalendarContract.Events.TITLE,
+                        CalendarContract.Events.EVENT_LOCATION,
+                        CalendarContract.Events.DTSTART,
+                        CalendarContract.Events.DTEND,
+                };
+
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+        String selection = CalendarContract.Events.EVENT_LOCATION + " = ? ";
+        String[] selectionArgs = new String[]{"London"};
+
+        cur = cr.query(uri, mProjection, selection, selectionArgs, null);
+
+        while (cur.moveToNext()) {
+            String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
+
+            listItems.add(title);
+
+            adapter.notifyDataSetChanged();
+        }
+
+    }
+
+
+
 }
