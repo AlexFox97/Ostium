e45380620cda88af1594914de0831ff680414421
Alex Fox <1alexwfox@gmail.com>
2018-11-16 05:15:22 +0000

    Start calendar code and create service to handle calendar loading

--- app/src/main/java/apps/mobile/ostium/MainActivity.java
+++ app/src/main/java/apps/mobile/ostium/MainActivity.java
@@ -1,82 +1,41 @@
 package apps.mobile.ostium;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.support.v4.app.ActivityCompat;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
 
 
 public class MainActivity extends Activity {
 
-    final ListView list = (ListView) findViewById(R.id.list);
 
-    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-    ArrayList<String> listItems=new ArrayList<String>();
-
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    ArrayAdapter<String> adapter;
-
-
-
+//Check permissions in activities before any calls are made
+/*
     private static final int MY_CAL_REQ = 1;
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED)
+    {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, MY_CAL_REQ);
+    }
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adapter=new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
-
-        list.setAdapter(adapter);
     }
 
+    private class CalendaresultReceiver extends ResultReceiver {
 
-
-    public void getDataFromEventTable(View v) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, MY_CAL_REQ);
+        public CalendaresultReceiver(Handler handler) {
+            super(handler);
         }
-        Cursor cur = null;
-        ContentResolver cr = getContentResolver();
 
-        String[] mProjection =
-                {
-                        "_id",
-                        CalendarContract.Events.TITLE,
-                        CalendarContract.Events.EVENT_LOCATION,
-                        CalendarContract.Events.DTSTART,
-                        CalendarContract.Events.DTEND,
-                };
-
-        Uri uri = CalendarContract.Events.CONTENT_URI;
-        String selection = CalendarContract.Events.EVENT_LOCATION + " = ? ";
-        String[] selectionArgs = new String[]{"London"};
-
-        cur = cr.query(uri, mProjection, selection, selectionArgs, null);
-
-        while (cur.moveToNext()) {
-            String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
-
-            listItems.add(title);
-
-            adapter.notifyDataSetChanged();
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            //TODO: Handle data received from service
         }
 
     }
-
-
-
 }
---
+++ app/src/main/java/apps/mobile/ostium/androidCalendarHandle.java
@@ -0,0 +1,75 @@
+package apps.mobile.ostium;
+
+import android.app.IntentService;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.provider.CalendarContract;
+
+
+public class androidCalendarHandle extends IntentService {
+
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public androidCalendarHandle(String name) {
+        super(name);
+    }
+
+    public androidCalendarHandle() {
+        super("androidCalendarHandle");
+    }
+
+    //Invoked on Intent Service Start
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        //Get ResultReceiver from the Intent passed to service
+        final ResultReceiver receiver = intent.getParcelableExtra("receiver");
+
+        //Create bundle to store data to send back
+        Bundle bundle = new Bundle();
+
+        /*
+        * Send data back to UI
+        * TODO: Implement success and fail cases for receiver.send
+        * */
+        receiver.send(1, bundle);
+
+    }
+
+    public void getDataFromEventTable() {
+
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+
+        String[] mProjection =
+                {
+                        "_id",
+                        CalendarContract.Events.TITLE,
+                        CalendarContract.Events.EVENT_LOCATION,
+                        CalendarContract.Events.DTSTART,
+                        CalendarContract.Events.DTEND,
+                };
+
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+        String selection = CalendarContract.Events.EVENT_LOCATION + " = ? ";
+        String[] selectionArgs = new String[]{"London"};
+
+        cur = cr.query(uri, mProjection, selection, selectionArgs, null);
+
+        while (cur.moveToNext()) {
+
+            //EXAMPLE: Get title
+            String title = cur.getString(cur.getColumnIndex(CalendarContract.Events.TITLE));
+
+            //TODO: Handle data
+        }
+    }
+}
