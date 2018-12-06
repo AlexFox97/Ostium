71ea30dfd937cac62dfaf4630fc3d4d4a84023b5
bacalla <b5021766@my.shu..ac.uk>
2018-12-06 19:53:15 +0000

    More UI updates
    Cards almost done, still need nested recylcer view finishing

--- app/src/main/java/apps/mobile/ostium/MainActivity.java
+++ app/src/main/java/apps/mobile/ostium/MainActivity.java
@@ -2,17 +2,19 @@ package apps.mobile.ostium;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //        Main Activity
 //        Home Activity
@@ -24,16 +26,17 @@ import android.widget.Toast;
 //        Dev Activity Three
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LogTagClass = MainActivity.class.getSimpleName();
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
-    private static final String LogTagClass = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // region Drawer - onCreate
         dl = (DrawerLayout) findViewById(R.id.activity_main);
         t = new ActionBarDrawerToggle(this, dl, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
@@ -43,42 +46,102 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         nv = (NavigationView) findViewById(R.id.nv);
-        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch (id) {
-                    case R.id.nav_home:
-                        gotoHome(nv);
-                        break;
-                    case R.id.nav_location:
-                        gotoLocation(nv);
-                        break;
-                    case R.id.nav_map:
-                        gotoMap(nv);
-                        break;
-                    case R.id.nav_settings:
-                        gotoSettings(nv);
-                        break;
-                    case R.id.nav_dev_one:
-                        gotoDevOne(nv);
-                        break;
-                    case R.id.nav_dev_two:
-                        gotoDevTwo(nv);
-                        break;
-                    case R.id.nav_dev_three:
-                        gotoDevThree(nv);
-                        break;
-                    default:
-                        return true;
+        nv.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.nav_home:
+                                gotoHome(nv);
+                                break;
+                            case R.id.nav_location:
+                                gotoLocation(nv);
+                                break;
+                            case R.id.nav_map:
+                                gotoMap(nv);
+                                break;
+                            case R.id.nav_settings:
+                                gotoSettings(nv);
+                                break;
+                            case R.id.nav_dev_one:
+                                gotoDevOne(nv);
+                                break;
+                            case R.id.nav_dev_two:
+                                gotoDevTwo(nv);
+                                break;
+                            case R.id.nav_dev_three:
+                                gotoDevThree(nv);
+                                break;
+                            default:
+                                return true;
+                        }
+                        return false;
+                    }
                 }
-                return false;
-            }
-        });
 
 
+        );
+        // endregion Drawer
+
+        // region CardRecycler - onCreate
+        RecyclerView recCardList = (RecyclerView) findViewById(R.id.cardList);
+        recCardList.setHasFixedSize(true);
+        LinearLayoutManager llmCard = new LinearLayoutManager(this);
+        llmCard.setOrientation(LinearLayoutManager.VERTICAL);
+        recCardList.setLayoutManager(llmCard);
+
+        CardAdapter ca = new CardAdapter(createCardList(8));
+        recCardList.setAdapter(ca);
+
+        // region ListRecycler - onCreate
+        RecyclerView recTagList = (RecyclerView) recCardList.findViewById(R.id.tagList);
+        recTagList.setHasFixedSize(true);
+        LinearLayoutManager llmTag = new LinearLayoutManager(this);
+        llmTag.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recTagList.setLayoutManager(llmTag);
+
+        TagAdapter ta = new TagAdapter(createTagList(8));
+        recTagList.setAdapter(ta);
+
+        // endregion Recycler
+
+//        //region extra recyclers
+//        // region Recycler - onCreate
+//        RecyclerView recList1 = (RecyclerView) findViewById(R.id.cardList2);
+//        recList1.setHasFixedSize(true);
+//        LinearLayoutManager llm1 = new LinearLayoutManager(this);
+//        llm.setOrientation(LinearLayoutManager.VERTICAL);
+//        recList1.setLayoutManager(llm1);
+//
+//        CardAdapter ca1 = new CardAdapter(createCardList(3));
+//        recList1.setAdapter(ca1);
+//
+//        // region Recycler - onCreate
+//        RecyclerView recList2 = (RecyclerView) findViewById(R.id.cardList3);
+//        recList2.setHasFixedSize(true);
+//        LinearLayoutManager llm2 = new LinearLayoutManager(this);
+//        llm.setOrientation(LinearLayoutManager.VERTICAL);
+//        recList2.setLayoutManager(llm);
+//
+//        CardAdapter ca2 = new CardAdapter(createCardList(3));
+//        recList2.setAdapter(ca2);
+//
+//        // region Recycler - onCreate
+//        RecyclerView recList3 = (RecyclerView) findViewById(R.id.cardList4);
+//        recList3.setHasFixedSize(true);
+//        LinearLayoutManager llm3 = new LinearLayoutManager(this);
+//        llm.setOrientation(LinearLayoutManager.VERTICAL);
+//        recList3.setLayoutManager(llm3);
+//
+//        CardAdapter ca3 = new CardAdapter(createCardList(3));
+//        recList3.setAdapter(ca3);
+
+
+        //endregion extra recyclers
     }
 
+    //region Drawer
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -121,6 +184,37 @@ public class MainActivity extends AppCompatActivity {
     public void gotoDevThree(View view) {
         Log.d(LogTagClass, "Button Dev Three clicked!");
         startActivity(new Intent(this, DevActivityThree.class));
+    }
+    //endregion Drawer
+
+    private List<CardInfo> createCardList(int size) {
+
+        List<CardInfo> result = new ArrayList<CardInfo>();
+        for (int i = 1; i <= size; i++) {
+            CardInfo ci = new CardInfo();
+//            ci.name = (CardInfo.NAME_PREFIX) + " title title title title title title title title title title title title title title title title title title title title title title title title title title title title title title title title " + i;
+//            ci.surname = CardInfo.SURNAME_PREFIX + " content content content content content content content content content content content content content content content content content content content content content content content"+ i;
+//            ci.email = CardInfo.EMAIL_PREFIX + " other other other other other other other other other other other other other other other other other other other other other other other other other other other other other other other other other other "+i + "@test.com";
+            ci.title = "Buy Almond milk, bread and bananas";
+            ci.details = "get gluten free bread!!";
+            ci.date = "03/12/2018";
+            result.add(ci);
+
+        }
+
+        return result;
+    }
+
+    private List<TagInfo> createTagList(int size) {
+
+        List<TagInfo> result = new ArrayList<TagInfo>();
+        for (int i = 1; i <= size; i++) {
+            TagInfo ci = new TagInfo();
+            ci.locationName = "Shops";
+            result.add(ci);
+        }
+
+        return result;
     }
 
 
