30383cd49632b2fc815a4104c17fa646de67b8ab
Katherine Martin <martin-katherine@hotmail.com>
2019-02-27 13:54:37 +0000

    Updated saving to include colour picker (Code is in Dev3) and icons in nav drawer

--- app/src/main/java/apps/mobile/ostium/DevActivityThree.java
+++ app/src/main/java/apps/mobile/ostium/DevActivityThree.java
@@ -1,11 +1,33 @@
 package apps.mobile.ostium;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorChangedListener;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
 
 public class DevActivityThree extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String COLOUR = "colour";
+
+    private int col;
+    private TextView t;
+    private TextView s;
+    private Button applyTextButton;
+    public int colourPickerColour;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +35,9 @@ public class DevActivityThree extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        t = findViewById(R.id.textView);
+        s = findViewById(R.id.colourTextview);
     }
 
     @Override
@@ -21,4 +46,71 @@ public class DevActivityThree extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.activity_location_toolbar, menu);
         return true;
     }
+
+    public void colourPickerDialogue(View view) {
+        ThemeActivity r = new ThemeActivity();
+        setContentView(R.layout.activity_theme);
+    }
+
+    public void buttonColourPickerDialogue(View view) {
+
+        ColorPickerDialogBuilder
+                .with(this)
+                .setTitle("Choose Colour")
+                .initialColor(R.style.page_background_red)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .showColorPreview(true)
+                .showColorEdit(true)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                        //toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                        setTitle(selectedColor);
+                        changeBackgroundColour(selectedColor);
+                    }
+                })
+                .setOnColorChangedListener(new OnColorChangedListener() {
+                    @Override
+                    public void onColorChanged(int i) {
+                        setTitle(i);
+                        changeBackgroundColour(i);
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        changeBackgroundColour(selectedColor);
+                        colourPickerColour = selectedColor;
+                        //saveInfo();
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+    }
+
+    private void changeBackgroundColour(int selectedColour)
+    {
+        Toast.makeText(getApplicationContext(), "Selected Colour: "+selectedColour, Toast.LENGTH_SHORT);
+        //setTheme(R.style.Theme_Ostium_Dark);
+
+        Button r = (Button) findViewById(R.id.button);
+        r.setBackgroundColor(selectedColour);
+
+        Button d = (Button) findViewById(R.id.button1);
+        d.setBackgroundColor(selectedColour);
+
+        //TextView q = (TextView) findViewById(R.id.nav_dev_two_toolbar);
+        //q.setBackgroundColor(selectedColour);
+
+        s.append("\n " + "Selected Colour: "+selectedColour);
+
+    }
+
+
 }
