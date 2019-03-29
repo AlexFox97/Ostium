580771e3a9428060807cfdaf809171ac8b3cbae9
abacall <25009151+abacall@users.noreply.github.com>
2019-03-29 13:56:20 +0000

    Revert "Stuff"

--- app/src/main/java/apps/mobile/ostium/Activity/SettingActivity.java
+++ app/src/main/java/apps/mobile/ostium/Activity/SettingActivity.java
@@ -47,7 +47,7 @@ public class SettingActivity extends AppCompatActivity implements RecyclerViewAd
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main_toolbar, menu);
+        getMenuInflater().inflate(R.menu.activity_settings_toolbar, menu);
         return true;
     }
 
